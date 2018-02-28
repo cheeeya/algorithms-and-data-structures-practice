0d2497ccb07e77f20f6cb770ab75854032769ff4
Samuel Chia <cheeeya@berkeley.edu>
2018-02-27 22:40:16 -0800

    implement basic hashing functions

--- hash_map_lru_cache/Samuel_Chia/lib/p02_hashing.rb
+++ hash_map_lru_cache/Samuel_Chia/lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    array_hash = 685495564
+    self.each_with_index do |el, idx|
+      if el.is_a?(Integer)
+        array_hash += el.hash * idx.hash
+      elsif el.is_a?(String)
+        array_hash += (el.ord * -2018595405) * idx.hash
+      end
+    end
+    array_hash
   end
 end
 
 class String
   def hash
+    return -1623050269 if self.empty?
+    self.split("").hash
   end
 end
 
@@ -16,6 +27,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_num = 1043638799
+    self.each do |key, val|
+      hash_num += (key.to_s.hash + 1837873649) * val.to_s.hash
+    end
+    hash_num
   end
 end
