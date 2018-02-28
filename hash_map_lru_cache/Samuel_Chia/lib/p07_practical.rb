d90e9ecca3f0397d69e8ee0ded1a0c1b5ba549f4
Samuel Chia <cheeeya@berkeley.edu>
2018-02-28 03:42:23 -0800

    no double iteration through linkedlist to check for inclusion

--- hash_map_lru_cache/Samuel_Chia/lib/p07_practical.rb
+++ hash_map_lru_cache/Samuel_Chia/lib/p07_practical.rb
@@ -3,7 +3,8 @@ require_relative 'p05_hash_map'
 def can_string_be_palindrome?(string)
   map = HashMap.new
   string.chars.each do |letter|
-    map.include?(letter) ? map.delete(letter) : map[letter] = true
+    # map.include?(letter) ? map.delete(letter) : map[letter] = true
+    map[letter] = true if map.delete(letter).nil?
   end
   map.count < 2
 end
