9c84221607908031489873abac978ebb1cbf77ac
Samuel Chia <cheeeya@berkeley.edu>
2018-02-28 03:39:03 -0800

    implement can_string_be_palindrome with hashmap, O(n) time

--- hash_map_lru_cache/Samuel_Chia/lib/p07_practical.rb
+++ hash_map_lru_cache/Samuel_Chia/lib/p07_practical.rb
@@ -1,5 +1,9 @@
 require_relative 'p05_hash_map'
 
-def can_string_be_palindrome?(string) 
-  
+def can_string_be_palindrome?(string)
+  map = HashMap.new
+  string.chars.each do |letter|
+    map.include?(letter) ? map.delete(letter) : map[letter] = true
+  end
+  map.count < 2
 end
