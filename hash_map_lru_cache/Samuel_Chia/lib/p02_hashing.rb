cb9d55ec7f3645474bfbadac6e68d4ea1ed3b29e
Samuel Chia <cheeeya@berkeley.edu>
2018-02-27 23:26:44 -0800

    refactor hash functions

--- hash_map_lru_cache/Samuel_Chia/lib/p02_hashing.rb
+++ hash_map_lru_cache/Samuel_Chia/lib/p02_hashing.rb
@@ -7,9 +7,9 @@ class Array
     array_hash = 685495564
     self.each_with_index do |el, idx|
       if el.is_a?(Integer)
-        array_hash += el.hash * idx.hash
+        array_hash += (el.to_s(2).to_i).hash * idx.hash
       elsif el.is_a?(String)
-        array_hash += (el.ord * -2018595405) * idx.hash
+        array_hash += el.unpack("B*")[0].to_i.hash * idx.hash
       end
     end
     array_hash
@@ -18,8 +18,7 @@ end
 
 class String
   def hash
-    return -1623050269 if self.empty?
-    self.split("").hash
+    [self].hash - 685495564
   end
 end
 
@@ -27,7 +26,7 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    hash_num = 1043638799
+    hash_num = 104363879
     self.each do |key, val|
       hash_num += (key.to_s.hash + 1837873649) * val.to_s.hash
     end
