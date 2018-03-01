9a7561c748813957bb8f75281a7f6441a3b4e2a4
Samuel Chia <cheeeya@berkeley.edu>
2018-03-01 04:08:22 -0800

    implement heapsort with a binary max heap

--- heaps/Samuel_Chia/lib/heap_sort.rb
+++ heaps/Samuel_Chia/lib/heap_sort.rb
@@ -2,5 +2,13 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    prc = Proc.new { |a,b| -1 * (a <=> b) }
+    self.length.times do |i|
+      BinaryMinHeap.heapify_up(self, i, i + 1, &prc)
+    end
+    self.length.times do |i|
+      self[0], self[self.length - i - 1] = self[length - i- 1], self[0]
+      BinaryMinHeap.heapify_down(self, 0, self.length - i - 1, &prc)
+    end
   end
 end
