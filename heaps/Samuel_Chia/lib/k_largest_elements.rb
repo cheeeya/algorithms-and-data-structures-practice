5fc9993987217d4333becd6a857ddc3f12faf3fd
Samuel Chia <cheeeya@berkeley.edu>
2018-03-01 04:42:12 -0800

    remove old code from k_largest

--- heaps/Samuel_Chia/lib/k_largest_elements.rb
+++ heaps/Samuel_Chia/lib/k_largest_elements.rb
@@ -1,14 +1,10 @@
 require_relative 'heap'
 
 def k_largest_elements(array, k)
-  prc = Proc.new { |a,b| -1 * (a <=> b) }
   min_heap = BinaryMinHeap.new
-  largest = []
   array.each do |el|
     min_heap.push(el)
-    if (min_heap.count > k)
-      min_heap.extract
-    end
+    min_heap.extract if min_heap.count > k
   end
   min_heap.store
 end
