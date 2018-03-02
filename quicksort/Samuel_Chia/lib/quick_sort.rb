0a12b294471c6a0aced7119f50535d379d9c91e9
Samuel Chia <cheeeya@berkeley.edu>
2018-03-01 22:34:13 -0800

    implement array quicksort

--- quicksort/Samuel_Chia/lib/quick_sort.rb
+++ quicksort/Samuel_Chia/lib/quick_sort.rb
@@ -4,12 +4,41 @@ class QuickSort
 
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
+    return array if array.length < 2
+    pivot_idx = rand(array.length)
+    pivot = array[pivot_idx]
+    left, right = Array.new
+    array.each_with_index do |el, idx|
+      next if idx == pivot_idx
+      case el <=> pivot
+      when 1
+        right << el
+      else
+        left << el
+      end
+    end
+    return QuickSort.sort1(left) + [pivot] + QuickSort.sort1(right)
   end
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    return if length < 2
+    pivot_idx = QuickSort.partition(array, start, length, &prc)
+    QuickSort.sort2!(array, start, pivot_idx - start, &prc)
+    QuickSort.sort2!(array, pivot_idx + 1, length - pivot_idx - 1, &prc)
   end
 
   def self.partition(array, start, length, &prc)
+    prc ||= Proc.new { |a, b| a <=> b }
+    boundary = start
+    (start + 1).upto(start + length - 1) do |i|
+      case prc.call(array[i], array[start])
+      when -1
+        array[i], array[boundary + 1] = array[boundary + 1], array[i]
+        boundary += 1
+      end
+    end
+    array[start], array[boundary] = array[boundary], array[start]
+    boundary
   end
 end
