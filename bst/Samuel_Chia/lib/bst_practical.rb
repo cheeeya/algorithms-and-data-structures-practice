b32baf6e0849f01de08290463b4db6d37ebe5655
Samuel Chia <cheeeya@berkeley.edu>
2018-03-05 04:25:23 -0800

    kth largest O(n)

--- bst/Samuel_Chia/lib/bst_practical.rb
+++ bst/Samuel_Chia/lib/bst_practical.rb
@@ -1,3 +1,12 @@
 
 def kth_largest(tree_node, k)
+  result = reverse_traverse(tree_node, [])
+  result[k - 1]
+end
+
+def reverse_traverse(node, arr)
+  reverse_traverse(node.right, arr) if node.right
+  arr << node
+  reverse_traverse(node.left, arr) if node.left
+  arr
 end
