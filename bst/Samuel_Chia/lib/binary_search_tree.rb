68c53b4d454bc0277725ad98b51713aaff9c8c5d
Samuel Chia <cheeeya@berkeley.edu>
2018-03-05 02:27:08 -0800

    implement insert and find, O(log n)

--- bst/Samuel_Chia/lib/binary_search_tree.rb
+++ bst/Samuel_Chia/lib/binary_search_tree.rb
@@ -1,14 +1,28 @@
-# There are many ways to implement these methods, feel free to add arguments 
+# There are many ways to implement these methods, feel free to add arguments
 # to methods as you see fit, or to create helper methods.
+require 'bst_node'
 
 class BinarySearchTree
+  attr_reader :root
   def initialize
+    @root = nil
   end
 
   def insert(value)
+    return @root = BSTNode.new(value) if @root.nil?
+    insert_node(value, @root)
   end
 
   def find(value, tree_node = @root)
+    return nil if tree_node.nil?
+    case tree_node.value <=> value
+    when 0
+      tree_node
+    when 1
+      find(value, tree_node.left)
+    when -1
+      find(value, tree_node.right)
+    end
   end
 
   def delete(value)
@@ -19,7 +33,7 @@ class BinarySearchTree
   end
 
   def depth(tree_node = @root)
-  end 
+  end
 
   def is_balanced?(tree_node = @root)
   end
@@ -30,5 +44,21 @@ class BinarySearchTree
 
   private
   # optional helper methods go here:
+  def insert_node(value, node)
+    case node.value <=> value
+    when 1
+      if node.left.nil?
+        node.left = BSTNode.new(value)
+      else
+        insert_node(value, node.left)
+      end
+    when -1
+      if node.right.nil?
+        node.right = BSTNode.new(value)
+      else
+        insert_node(value, node.right)
+      end
+    end
+  end
 
 end
--- bst/Samuel_Chia/lib/bst_node.rb
+++ bst/Samuel_Chia/lib/bst_node.rb
@@ -1,4 +1,6 @@
 class BSTNode
+  attr_accessor :value, :left, :right
   def initialize(value)
+    @value = value
   end
 end
